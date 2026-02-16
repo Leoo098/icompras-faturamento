bb3dba09eba1ebbf99d5571bb0691675105c4ec1
Leonardo Ribeiro <leonardor716@gmail.com>
2026-02-16 17:10:13 -0300

    Ambiente inicial de faturamento do projeto icompras
    - Configuração do MinIO
    - Criação das classes para manipulação do bucket

---
+++ src/test/java/leo_dev/icompras/faturamento/FaturamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package leo_dev.icompras.faturamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaturamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
